8e7ac1a4a01fe3bbb3ce2da44740d9c2f7e2878a
Tim Sheerman-Chase <orders2008@sheerman-chase.org.uk>
2015-01-01 17:49:53 +0000

    Tidy

--- app.py
+++ app.py
@@ -4,25 +4,24 @@ import web, os
 import sqlite3
 
 urls = (
-	'/.*', 'hello',
+	'/.*', 'TestPage',
 	)
 
-class hello:
+class TestPage:
 	def GET(self):
 		db = web.ctx.db
 		results = db.select("data", limit = 100)
 		names = [tmp["name"] for tmp in results]
 
-		return "Hello, world." + (",<br/>".join(names)) + "..."
+		return "<br/>".join(names)
 
-def my_loadhook():
+def InitDatabaseConn():
 	curdir = os.path.dirname(__file__)
-	conn = web.database(dbn='sqlite', db=os.path.join(curdir, 'auxgis.db'))
-	web.ctx.db = conn
+	web.ctx.db = web.database(dbn='sqlite', db=os.path.join(curdir, 'auxgis.db'))
 
 app = web.application(urls, globals())
 curdir = os.path.dirname(__file__)
-app.add_processor(web.loadhook(my_loadhook))
+app.add_processor(web.loadhook(InitDatabaseConn))
 session = web.session.Session(app, web.session.DiskStore(os.path.join(curdir,'sessions')),)
 
 application = app.wsgifunc()
