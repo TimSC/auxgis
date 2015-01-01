71898b408feaf4b307a15b8416504eee3556a417
Tim Sheerman-Chase <orders2008@sheerman-chase.org.uk>
2015-01-01 17:26:41 +0000

    Use sqlite3 select

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Recipe for apache: http://webpy.org/cookbook/mod_wsgi-apache
 
 import web, os
+import sqlite3
 
 urls = (
 	'/.*', 'hello',
@@ -8,12 +9,39 @@ urls = (
 
 class hello:
 	def GET(self):
-			return "Hello, world."
+		cur = web.ctx.db.cursor()
+		cur.execute("SELECT name FROM data;")
+		tmp = []
+		count = 0
+
+		while True:
+		  
+			row = cur.fetchone()
+			
+			if row == None:
+				break
+				
+			tmp.append(row[0])
+			count += 1
+			if count > 100:
+				break
+
+
+		return "Hello, world." + (",".join(tmp[:100])) + "..."
+
+def my_loadhook():
+	curdir = os.path.dirname(__file__)
+	conn = sqlite3.connect(os.path.join(curdir, 'auxgis.db'))
+	web.ctx.db = conn
 
 app = web.application(urls, globals())
 
 curdir = os.path.dirname(__file__)
+
+app.add_processor(web.loadhook(my_loadhook))
+
 session = web.session.Session(app, web.session.DiskStore(os.path.join(curdir,'sessions')),)
+
 
 application = app.wsgifunc()
 
