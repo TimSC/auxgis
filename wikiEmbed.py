fd48c25c58a84bfecdd929aabaceb93a6103747a
Tim <tim2009@sheerman-chase.org.uk>
2015-01-11 19:04:34 +0000

    Cache wikipedia articles

--- wikiEmbed.py
+++ wikiEmbed.py
@@ -1,18 +1,55 @@
-import urllib2, json
+import urllib2, json, sqlitedict, time, os
 import mwparserfromhell
 
 class MediawikiArticle(object):
-	def __init__(self, articleName):
+	def __init__(self, articleName, enableCache=True):
+
+		self.text = None
+
+		if enableCache:
+			curdir = os.path.dirname(__file__)
+			self.cache = sqlitedict.SqliteDict(os.path.join(curdir, 'WikipediaArticles.db'), autocommit=False)
+		else:
+			self.cache = None
+
+		if self.cache is not None:
+
+			#Check if photo is available in cache			
+			available = articleName in self.cache
+			if available:
+				recordTime, recordData = self.cache[articleName]
+				ageSec = time.time() - recordTime
+
+				#If not too old
+				if ageSec < 24*60*60:
+					#Use cached version
+					self._ExtractFields(recordData)
+					return
+
+		self._RetrieveViaWeb(articleName)
+
+	def __del__(self):
+		if self.cache is not None:
+			self.cache.commit()
+
+	def _RetrieveViaWeb(self, articleName):
 
 		url = u"https://en.wikipedia.org/w/api.php?format=json&action=query&titles={0}&prop=revisions&rvprop=content&continue=".format(urllib2.quote(articleName))
 		#print url
 		ha = urllib2.urlopen(url)
 		result = ha.read()
-		print result
+		#print result
+
+		self._ExtractFields(result)
+
+		if self.cache is not None:
+			self.cache[articleName] = (time.time(), result)
+
+	def _ExtractFields(self, result):
+
 		decodedResult = json.loads(result)	
 
 		pageIds = decodedResult["query"]["pages"].keys()
-		self.text = None
 
 		if pageIds > 0:
 			pageData = decodedResult["query"]["pages"][pageIds[0]]
